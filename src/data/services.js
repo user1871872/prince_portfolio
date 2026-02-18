3334995d51637861383e8d77c438b59a50bde78d
user1871872 <ajd62874@gmail.com>
2026-02-18 16:20:41 +0800

    services updates

--- src/data/services.js
+++ src/data/services.js
@@ -3,8 +3,8 @@
  * Each item: { id, title, description }
  */
 export const services = [
-  { id: '1', title: 'Service title', description: 'Short description. Replace with your own.' },
-  { id: '2', title: 'Service title', description: 'Short description. Replace with your own.' },
-  { id: '3', title: 'Service title', description: 'Short description. Replace with your own.' },
-  { id: '4', title: 'Service title', description: 'Short description. Replace with your own.' },
+  { id: '1', title: 'Service title', description: 'Lorem ipsum dolor sit amet consectetur adipiscing elit.' },
+  { id: '2', title: 'Service title', description: 'Lorem ipsum dolor sit amet consectetur adipiscing elit.' },
+  { id: '3', title: 'Service title', description: 'Lorem ipsum dolor sit amet consectetur adipiscing elit.' },
+  { id: '4', title: 'Service title', description: 'Lorem ipsum dolor sit amet consectetur adipiscing elit.' },
 ];
